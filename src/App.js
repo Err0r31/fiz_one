04a6bfb57479d445f16d45be1fcb4ff86a28096d
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-11-30 02:38:02 +0300

    add animatedRoute

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProjectsPage from "./components/ProjectsPage/ProjectsPage";
-import './css/style.css';
+import AnimatedRoute from "./AnimatedRoute";
+import "./css/style.css";
+
 function App() {
   return (
     <Router>
       <div className="app">
         <div className="container">
-        <Routes>
-            <Route path="/" element={<ProjectsPage />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<AnimatedRoute><ProjectsPage /></AnimatedRoute>}/>
+          </Routes>
         </div>
       </div>
     </Router>
