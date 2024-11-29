efca7cfbf8dba9767111e7c175d3e6ecaf6d90b5
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-11-30 02:30:23 +0300

    add modal

--- src/components/ProjectsPage/ProjectsPage.jsx
+++ src/components/ProjectsPage/ProjectsPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/ProjectsPage.css';
+import ModalProjectsPage from './ModalProjectsPage';
 
 const projects = [
   {
@@ -8,8 +9,7 @@ const projects = [
     startDate: "2020-03-04",
     plannedEndDate: "2025-02-02",
     endDate: null,
-    projectPlans: [
-    ],
+    projectPlans: [],
   },
   {
     id: "d81c6da0-994b-4527-8800-f39632b148c1",
@@ -17,16 +17,40 @@ const projects = [
     startDate: "2021-05-10",
     plannedEndDate: "2022-10-01",
     endDate: "2022-09-15",
-    projectPlans: [
-    ],
+    projectPlans: [],
+  },
+  {
+    id: "d81c6da0-994b-4527-8800-f39632b148c2",
+    name: "Реконструкция моей дачи",
+    startDate: "2022-05-10",
+    plannedEndDate: "2023-10-01",
+    endDate: null,
+    projectPlans: [],
   },
 ];
 
 function ProjectsPage() {
+  const [modalProjectsPageOpen, setModalProjectsPageOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleClickProject = (project) => {
+    setSelectedProject(project); // Устанавливаем выбранный проект
+    setModalProjectsPageOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalProjectsPageOpen(false);
+    setSelectedProject(null);
+  };
+
   return (
     <div className="projects-page-wrapper">
       {projects.map((project) => (
-        <div key={project.id} className="project__block">
+        <div
+          key={project.id}
+          className="project__block"
+          onClick={() => handleClickProject(project)}
+        >
           <h2 className="project__block-title">{project.name}</h2>
           <p className="project__block-date">
             Начало проекта: {project.startDate}
@@ -39,6 +63,12 @@ function ProjectsPage() {
           </p>
         </div>
       ))}
+      {modalProjectsPageOpen && selectedProject && (
+        <ModalProjectsPage
+          project={selectedProject}
+          onClose={handleCloseModal}
+        />
+      )}
     </div>
   );
 }
