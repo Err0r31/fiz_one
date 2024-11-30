531a9aa965ceac659f9e2b40143c9f500af85f48
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-11-30 03:35:19 +0300

    restyle header

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Layout.css';
+import logoTeam from '../../assets/img/logo/logo-team.svg';
+import logoCompany from '../../assets/img/logo/logo-company.svg';
 
 function Layout({ PageTitle, children }) {
     return (
         <>
             <header className="header">
                 <div className="header__wrapper">
-                    <Link to='/' className="header__link">Просмотр<br /> проектов</Link>
-                    <Link to='/' className="header__link">Создание проекта</Link>
-                    <Link to='/' className="header__link header__link--black">Выход</Link>
+                    <div className="header__logo">
+                        <img className="header__logo-image logo-company" src={logoCompany} alt="Логотип компании"></img>
+                        <img className="header__logo-image logo-team" src={logoTeam} alt="Логотип команды Лидеры"></img>
+                    </div>
+                    <div className="header__nav">
+                        <Link to='/' className="header__link header__link--active">Просмотр проектов</Link>
+                        <Link to='/' className="header__link">Создание проекта</Link>
+                        <Link to='/' className="header__link header__link--black">Выход</Link>
+                    </div>
                 </div>
             </header>
             <main>
